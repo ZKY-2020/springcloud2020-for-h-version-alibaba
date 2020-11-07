e614924ed229860ba8c889ea6cca67191a14c1da
ZKY-2020 <574086634@qq.com>
2020-11-07 10:17:16 +0800

    增加查询

--- cloud2020-consumer-feign-order80/src/main/java/com/atguigu/springcloud/config/FeignConfig.java
+++ cloud2020-consumer-feign-order80/src/main/java/com/atguigu/springcloud/config/FeignConfig.java
@@ -1,10 +1,12 @@
 package com.atguigu.springcloud.config;
 
 import feign.Logger;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class FeignConfig {
     @Bean
     Logger.Level feignLoggerLevel(){
